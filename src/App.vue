8d3b09ef809e5e0ca8b01dc087bde356117a6db6
JosephO1992 <josephraoreilly@gmail.com>
2021-11-05 10:43:58 +0000

    added branch for preso

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-</script>
+                                                                    </script>
 
 <template>
   <div class="container">
     <img alt="Vue logo" src="./assets/nimbus-hosting.svg" />
-    <HelloWorld msg="Hey Nimbus Team!" />
+    <HelloWorld msg="Hey Nimbus Team! On a new-branch" />
   </div>
 </template>
 
